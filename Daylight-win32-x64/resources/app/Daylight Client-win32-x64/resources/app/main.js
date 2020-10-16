35a8cd2c3605dddc1ab34cb08dadab143ca598db
userdaylightinc <usedaylightinc@gmail.com>
2020-10-16 11:28:08 -0700

    0.0.4

---
+++ Daylight-win32-x64/resources/app/Daylight Client-win32-x64/resources/app/main.js
@@ -0,0 +1,91 @@
+const { app, BrowserWindow, ipcMain, Menu, autoUpdater, dialog } = require('electron');
+if(require('electron-squirrel-startup')) return;
+let mainWindow;
+if(require('electron-squirrel-startup') == true ) app.quit();
+
+const server = 'https://update.electronjs.org'
+const url = `${server}/userdaylightinc/Daylight-Main/${process.platform}-${process.arch}/${app.getVersion()}`
+
+
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 10000)
+
+
+
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile(`${__dirname}/pages/home.html`)
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+  mainWindow.webContents.openDevTools()
+
+
+  Menu.setApplicationMenu(null)
+  
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: require(__dirname + "/package.json").version });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
